3c05013544f2fbce80066eabc17067e70bee4487
Gudivni <tamerlan.almazbekov@gmail.com>
2023-03-05 20:59:49 +0600

    add

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from product.models import Product, Comment
 from product.forms import ProductCreateForm, CommentCreateForm
+from product.constans import PAGINATION_LIMIT
 
 # Create your views here.
 
@@ -12,7 +13,20 @@ def main_view(request):
 
 def products_view(request):
     if request.method == 'GET':
-        products = Product.objects.all()
+        products = Product.objects.all().order_by('-created_date')
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+
+        if search:
+            products = products.filter(title__contains=search) | products.filter(description__contains=search)
+
+        max_page = products.__len__() / PAGINATION_LIMIT
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+
+        products = products[PAGINATION_LIMIT * (page - 1): PAGINATION_LIMIT * page]
 
         context = {
             'products': [
@@ -24,7 +38,9 @@ def products_view(request):
                     'hashtags': product.hashtags.all()
                 }
                 for product in products
-            ]
+            ],
+            'user': request.user,
+            'pages': range(1, max_page+1)
         }
 
         return render(request, 'products/products.html', context=context)
